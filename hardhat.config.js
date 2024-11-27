9c15cb23023ed012ec782fb599816b2c6660f4b3
gungho0619 <tzztntt@gmail.com>
2024-11-27 23:08:12 +0900

    chore: environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.6.12",
+  solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/5e14b8949f6b473f9cfc5adc98f1704f`,
+    qan: {
+      url: `https://rpc-testnet-dev.qanplatform.com`,
 
       accounts: [
-        "0xf7b4bc954f77ea129f4be607d12440eef972646425a8f440590d30234b3bec4c",
+        "YOUR_PRIVATE_KEY_HERE",
       ],
     },
   },
